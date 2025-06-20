69ebb8db9e74f1f73b291065a4d20aa9b367edd2
nirojkhadka123 <khadkaniroj962@gmail.com>
2025-06-20 08:57:53 +0545

    initial updates

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    center: true,
+    items: 3.5,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+})
